37b250a64384e37ea3991f8139606a57ee258af8
Mhavra <mavra.villin@gmail.com>
2026-06-19 20:22:34 -0400

    agregando css y otros
    se agregaron cosas y se andan modificando las rutas por que estan mal, pero despues le sigo

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css';
+import App from './app';
+import 'styles/index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
